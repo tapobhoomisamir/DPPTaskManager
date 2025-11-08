9724b23683367b5794e68fdcac7b6ecf5883289a
satya <satya0307@yahoo.co.in>
2025-11-08 16:35:43 +0530

    minor changes and read me upated

---
+++ webapp/taskmanager/app/Commands/README.md
@@ -0,0 +1,7 @@
+
+
+Command to create the next 12 weeks
+To change the number of weeks and date formate in php script GenerateWorkweeks
+
+Command to execute the script
+>php spark generate:workweeks
--- webapp/taskmanager/app/Views/tasks/create.php
+++ webapp/taskmanager/app/Views/tasks/create.php
@@ -7,13 +7,13 @@
     <h1>Create Task</h1>
 
     <form action="/tasks/store" method="post">
-        <label>Title</label><br>
+        <label>Work list</label><br>
         <input type="text" name="title" required><br><br>
 
         <label>Description</label><br>
         <textarea name="description"></textarea><br><br>
 
-        <button type="submit">Save Task</button>
+        <button type="submit">Save Work List</button>
     </form>
 </body>
 </html>
